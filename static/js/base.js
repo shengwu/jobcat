aa6651f4b68523511a109328ebfd1465bd90645d
Sheng Wu <pkernoobie@gmail.com>
2013-08-24 22:50:01 -0700

    Add static resources

--- static/js/base.js
+++ static/js/base.js
@@ -2,4 +2,4 @@ var jobcat = angular.module('jobcat', []);
 jobcat.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
-});
+}]);
---
+++ static/js/home.js
@@ -0,0 +1,4 @@
+angular.module('jobcat').controller('Home', ['$scope', function($scope) {
+    $scope.studentSignup = false;
+    $scope.residentSignup = false;
+}]);
