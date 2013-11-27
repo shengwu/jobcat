9389a1dfc0c9e179076cac206c9af459ba7f954e
Sheng Wu <pkernoobie@gmail.com>
2013-11-27 00:19:52 -0600

    Rename files

--- jobcat/views.py
+++ jobcat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-    return render_to_response('home.html', context_instance=RequestContext(request))
+    return render_to_response('landing.html', context_instance=RequestContext(request))
 
 def jobs(request):
     return render_to_response('jobs.html', context_instance=RequestContext(request))
